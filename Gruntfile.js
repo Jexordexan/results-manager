f2b3b909f3f700f6cc114c25c53e938ff0f00436
mbarr <mbarr@onshape.com>
2017-03-22 16:39:51 -0400

    v0.0.2 - much saner results from GET requests, better organization and logging.

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,6 +36,14 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('gruntify-eslint');
   grunt.loadNpmTasks('grunt-forever');
 
+  grunt.registerTask('rebuild', 'Drop and rebuild database with index', function() {
+    const done = this.async();
+    const resultsManagerAPI = require('./lib/resultsManagerAPI');
+    resultsManagerAPI.rebuild(function() {
+      done();
+    });
+  });
+
   grunt.registerTask('start', [ 'forever:server:start' ]);
   grunt.registerTask('stop', [ 'forever:server:stop' ]);
   grunt.registerTask('restart', [ 'forever:server:restart' ]);
