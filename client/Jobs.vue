bb80f168c0a448808ac62f025d89be3adc345542
Jordan Simonds <jsimonds@onshape.com>
2017-04-25 15:51:07 -0400

    Add filtering to Jobs page

--- client/Jobs.vue
+++ client/Jobs.vue
@@ -3,8 +3,28 @@
     <v-progress-linear v-if="jobs.loading" success indeterminate height="4" class="top-loader"></v-progress-linear>
     <v-col xs12="xs12" class="pl-3" v-if="!jobs.loading">
       <h5 class="grey--text">Jobs</h5>
+      <v-divider></v-divider>
+      <v-row row>
+        <v-col xs5>
+          <v-text-field
+            name="filter-input"
+            label="Filter"
+            v-model="search.job"
+            prepend-icon="search">
+          </v-text-field>
+        </v-col>
+        <v-col xs1>
+          <v-subheader>Show:</v-subheader>
+        </v-col>
+        <v-col xs3>
+          <v-select :items="buildTypes" v-model="search.type"></v-select>
+        </v-col>
+        <v-col xs2>
+          <v-btn @click="clearFilters()" light flat large>Clear</v-btn>
+        </v-col>
+      </v-row>
       <v-list>
-        <v-list-item v-for="item in jobs.items" v-bind:key="item.job">
+        <v-list-item v-for="item in filterJobs(jobs.items)" v-bind:key="item.job">
           <v-list-tile avatar class="no-action">
             <v-list-tile-avatar>
               <v-icon v-if="item.lastFailed === 0" class="green--text">label</v-icon>
@@ -26,8 +46,19 @@
 <script>
 export default {
   name: 'Jobs',
-  data: function () {
+  data: function() {
     return {
+      buildTypes: [
+        {text: 'All jobs', value: ''},
+        {text: 'uitest', value: 'uitest'},
+        {text: 'precommit', value: 'precommit'},
+        {text: 'main', value: 'main'}
+      ],
+      search: {
+        job: '',
+        latest: '',
+        type: ''
+      },
       jobs: {
         loading: true
       }
@@ -44,6 +75,24 @@ export default {
           vm.jobs.loading = false
           return response.body
         })
+    },
+    filterJobs: function(items) {
+      return items.filter(item => {
+        for (let filterKey in this.search) {
+          const value = item[filterKey].toString();
+          let test = this.search[filterKey] || '';
+          if (typeof test === 'object') test = test.value;
+          if (value.indexOf(test) === -1) return false;
+        }
+        return true;
+      });
+    },
+    clearFilters: function() {
+      this.search = {
+        job: '',
+        latest: '',
+        type: ''
+      }
     }
   },
   created: function () {
